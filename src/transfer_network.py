3545d55ccbc7c0a0d0f765b38fb77c609aa71ffd
Clloud <chenshaofengsf@163.com>
2019-07-20 23:42:00 +0800

    update transfer_network

--- src/transfer_network.py
+++ src/transfer_network.py
@@ -4,7 +4,9 @@ from point import Point
 class TransferNetwork():
     def __init__(self, points, clusters):
         self.nodes = []
+        self.edges = []
         self.create_transfer_node(clusters)
+        self.create_transfer_edge(points, clusters)
 
     def create_transfer_node(self, clusters):
         for cluster in clusters:
@@ -14,3 +16,40 @@ class TransferNetwork():
                 sum2 += point.longitude
             node = Point(sum1 / len(cluster), sum2 / len(cluster))
             self.nodes.append(node)
+
+    # 二维矩阵保存 transfer edge ，矩阵的行和列均为 cluster的序号（无向图则该矩阵为对称的）
+    # -1 表示两个cluster之间没有边相连, >=0 的数值表示相连边属于哪一条路径（即trajectory_id）
+    def create_transfer_edge(self, points, clusters):
+        clusters_size = len(clusters)
+        # 初始化transfer edge矩阵
+        self.edges = [[-1 for col in range(clusters_size)] for row in range(clusters_size)]
+
+        for i in range(clusters_size):
+            for j in range(i + 1, clusters_size):
+                p_tid = -1
+                for point in clusters[i]:
+                    # 一条路径只使用一个点，因此cluster中有几条不同的路径则循环几次
+                    if point.trajectory_id == p_tid:
+                        continue
+                    p_tid = point.trajectory_id
+
+                    # 判断cluster[i]和cluster[j]是否有边相连
+                    # point是以时间序存储在cluster中的，因此可保证第一个检查到的边即为有效的transfer edge
+                    if self.trajectory_only_edge(p_tid, i, j) and \
+                        self.trajectory_pass_cluster(p_tid, clusters[j]):
+                        self.edges[i][j] = p_tid
+                        self.edges[j][i] = p_tid
+
+    # 检查这条路径在cluster上是否有point（考虑point.id？）
+    def trajectory_pass_cluster(self, tid, cluster):
+        for point in cluster:
+            if point.trajectory_id == tid:
+                return True
+        return False
+
+    # 保证一条路径在一个cluster上只有一条transfer edge（按时间序）
+    def trajectory_only_edge(self, tid, i, j):
+        for k in range(i + 1, j):
+            if self.edges[i][k] == tid:
+                return False
+        return True
--- tests/test_transfer_network.py
+++ tests/test_transfer_network.py
@@ -4,6 +4,19 @@ root_path = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 src_path = root_path + '\\src'
 sys.path.append(src_path)
 
-import transfer_network
+from transfer_network import TransferNetwork
+from point import Point
 
+a = TransferNetwork([],[
+        [Point(1, 3, 1), Point(3, 1, 1), Point(1, 1, 2), Point(2, 2, 2)],
+        [Point(5, 4, 2), Point(6, 3, 2), Point(6, 5, 3), Point(5, 4, 3)],
+        [Point(3, 0, 1), Point(4, -1, 1), Point(4, 0, 3), Point(2, -2, 3)]
+    ])
 
+for node in a.nodes:
+    print(node)
+
+for i in range(len(a.edges)):
+    for j in range(len(a.edges)):
+        if a.edges[i][j] != -1:
+            print("edge: ({}, {}) trajectory_id: {}".format(i, j, a.edges[i][j]))
